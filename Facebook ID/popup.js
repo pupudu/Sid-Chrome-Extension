944dd637685e5b714abd2b7568c77a707e374bd0
pupudu <pubudu_pincode@live.com>
2015-07-18 09:38:27 +0530

    Permissions defined. Call back function modified

--- Facebook ID/popup.js
+++ Facebook ID/popup.js
@@ -15,12 +15,12 @@ document.addEventListener('DOMContentLoaded', function() {
       i.value = tab.url;
 	  if(i.value.includes("facebook")){
 		
+		var x='document.body.style.backgroundColor="red";document.getElementsByClassName("_2dpb")[0].innerHTML;';
 		/*chrome.tabs.duplicate(tab.id);*/
 		chrome.tabs.executeScript(tab.id,{
-			code:'var x=10;x'
+			code:x;
 		  },function(d){
-			var str=document.getElementsByClassName("_c24 _50f3").innerHTML;
-			alert(str);
+			alert(d);
 		  });
 		  /*chrome.tabs.executeScript(tab.id,function(){
 			 document.body.style.backgroundColor="red"
@@ -28,6 +28,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	  }else{
 		alert("Incorrect Page");
 	  }
+	  
+	  /*chrome.permissions.request({
+          permissions: ['webRequest']
+        }, function(granted) {
+          // The callback argument will be true if the user granted the permissions.
+          if (granted) {
+            alert("granted");
+          } else {
+            alert("deined");
+          }
+        });*/
+	  
 	  
 	  /*
       f.appendChild(i);
@@ -37,9 +49,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	
   }, false);
-  chrome.browserAction.onClicked.addListener(function(tab) {
+  /*chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     code: 'document.body.style.backgroundColor="red";alert("dodan");'
   });
-});
+});*/
 }, false);
